dd54e4b0bc75b4ade06c30ee276e9cbf4033ec5f
Cedar Daniel <cedardaniel1998@gmail.com>
2020-11-09 14:13:21 +0100

    Implemented notifications and aggregation

--- models/likeModel.js
+++ models/likeModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Post = require('../models/postModel');
 
 const likeSchema = new mongoose.Schema(
   {
@@ -18,6 +19,37 @@ likeSchema.pre(/^find/, function (next) {
   });
 
   next();
+});
+
+likeSchema.statics.calcNumber = async function (postId) {
+  const stats = await this.aggregate([
+    {
+      $match: { post: postId },
+    },
+    {
+      $group: {
+        _id: '$post',
+        nLikes: { $sum: 1 },
+      },
+    },
+  ]);
+  // console.log(stats);
+
+  if (stats.length > 0) {
+    await Post.findByIdAndUpdate(postId, {
+      likesQuantity: stats[0].nLikes,
+    });
+  } else {
+    await Post.findByIdAndUpdate(postId, {
+      likesQuantity: 0,
+    });
+  }
+};
+
+likeSchema.post('save', function () {
+  //this always points to the current like
+  this.constructor.calcNumber(this.post);
+  //post middleware doesn't get access to next
 });
 
 const Like = mongoose.model('Like', likeSchema);
--- models/postModel.js
+++ models/postModel.js
@@ -14,6 +14,17 @@ const postSchema = new mongoose.Schema(
     category: {
       type: String,
     },
+
+    likesQuantity: {
+      type: Number,
+      default: 0,
+    },
+
+    commentsQuantity: {
+      type: Number,
+      default: 0,
+    },
+
     createdAt: {
       type: Date,
       default: Date.now(),
